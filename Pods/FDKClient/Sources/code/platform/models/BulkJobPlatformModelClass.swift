a01cea845fdb672bb712f210c533c652fa190293
rushangfynd <rushangprajapati@Rushangs-MacBook-Pro.local>
2022-08-24 13:06:16 +0530

    [Updated] Setup dynamic path for archive

---
+++ Pods/FDKClient/Sources/code/platform/models/BulkJobPlatformModelClass.swift
@@ -0,0 +1,277 @@
+
+
+import Foundation
+public extension PlatformClient {
+    /*
+         Model: BulkJob
+         Used By: Catalog
+     */
+
+    class BulkJob: Codable {
+        public var createdBy: UserInfo1?
+
+        public var customTemplateTag: String?
+
+        public var modifiedOn: String?
+
+        public var filePath: String?
+
+        public var trackingUrl: String?
+
+        public var failedRecords: [[String: Any]]?
+
+        public var createdOn: String
+
+        public var cancelledRecords: [[String: Any]]?
+
+        public var stage: String?
+
+        public var isActive: Bool?
+
+        public var succeed: Int?
+
+        public var companyId: Int
+
+        public var cancelled: Int?
+
+        public var failed: Int?
+
+        public var templateTag: String?
+
+        public var modifiedBy: UserInfo1?
+
+        public var total: Int
+
+        public enum CodingKeys: String, CodingKey {
+            case createdBy = "created_by"
+
+            case customTemplateTag = "custom_template_tag"
+
+            case modifiedOn = "modified_on"
+
+            case filePath = "file_path"
+
+            case trackingUrl = "tracking_url"
+
+            case failedRecords = "failed_records"
+
+            case createdOn = "created_on"
+
+            case cancelledRecords = "cancelled_records"
+
+            case stage
+
+            case isActive = "is_active"
+
+            case succeed
+
+            case companyId = "company_id"
+
+            case cancelled
+
+            case failed
+
+            case templateTag = "template_tag"
+
+            case modifiedBy = "modified_by"
+
+            case total
+        }
+
+        public init(cancelled: Int? = nil, cancelledRecords: [[String: Any]]? = nil, companyId: Int, createdBy: UserInfo1? = nil, createdOn: String, customTemplateTag: String? = nil, failed: Int? = nil, failedRecords: [[String: Any]]? = nil, filePath: String? = nil, isActive: Bool? = nil, modifiedBy: UserInfo1? = nil, modifiedOn: String? = nil, stage: String? = nil, succeed: Int? = nil, templateTag: String? = nil, total: Int, trackingUrl: String? = nil) {
+            self.createdBy = createdBy
+
+            self.customTemplateTag = customTemplateTag
+
+            self.modifiedOn = modifiedOn
+
+            self.filePath = filePath
+
+            self.trackingUrl = trackingUrl
+
+            self.failedRecords = failedRecords
+
+            self.createdOn = createdOn
+
+            self.cancelledRecords = cancelledRecords
+
+            self.stage = stage
+
+            self.isActive = isActive
+
+            self.succeed = succeed
+
+            self.companyId = companyId
+
+            self.cancelled = cancelled
+
+            self.failed = failed
+
+            self.templateTag = templateTag
+
+            self.modifiedBy = modifiedBy
+
+            self.total = total
+        }
+
+        required public init(from decoder: Decoder) throws {
+            let container = try decoder.container(keyedBy: CodingKeys.self)
+
+            do {
+                createdBy = try container.decode(UserInfo1.self, forKey: .createdBy)
+
+            } catch DecodingError.typeMismatch(let type, let context) {
+                print("Type '\(type)' mismatch:", context.debugDescription)
+                print("codingPath:", context.codingPath)
+            } catch {}
+
+            do {
+                customTemplateTag = try container.decode(String.self, forKey: .customTemplateTag)
+
+            } catch DecodingError.typeMismatch(let type, let context) {
+                print("Type '\(type)' mismatch:", context.debugDescription)
+                print("codingPath:", context.codingPath)
+            } catch {}
+
+            do {
+                modifiedOn = try container.decode(String.self, forKey: .modifiedOn)
+
+            } catch DecodingError.typeMismatch(let type, let context) {
+                print("Type '\(type)' mismatch:", context.debugDescription)
+                print("codingPath:", context.codingPath)
+            } catch {}
+
+            do {
+                filePath = try container.decode(String.self, forKey: .filePath)
+
+            } catch DecodingError.typeMismatch(let type, let context) {
+                print("Type '\(type)' mismatch:", context.debugDescription)
+                print("codingPath:", context.codingPath)
+            } catch {}
+
+            do {
+                trackingUrl = try container.decode(String.self, forKey: .trackingUrl)
+
+            } catch DecodingError.typeMismatch(let type, let context) {
+                print("Type '\(type)' mismatch:", context.debugDescription)
+                print("codingPath:", context.codingPath)
+            } catch {}
+
+            do {
+                failedRecords = try container.decode([[String: Any]].self, forKey: .failedRecords)
+
+            } catch DecodingError.typeMismatch(let type, let context) {
+                print("Type '\(type)' mismatch:", context.debugDescription)
+                print("codingPath:", context.codingPath)
+            } catch {}
+
+            createdOn = try container.decode(String.self, forKey: .createdOn)
+
+            do {
+                cancelledRecords = try container.decode([[String: Any]].self, forKey: .cancelledRecords)
+
+            } catch DecodingError.typeMismatch(let type, let context) {
+                print("Type '\(type)' mismatch:", context.debugDescription)
+                print("codingPath:", context.codingPath)
+            } catch {}
+
+            do {
+                stage = try container.decode(String.self, forKey: .stage)
+
+            } catch DecodingError.typeMismatch(let type, let context) {
+                print("Type '\(type)' mismatch:", context.debugDescription)
+                print("codingPath:", context.codingPath)
+            } catch {}
+
+            do {
+                isActive = try container.decode(Bool.self, forKey: .isActive)
+
+            } catch DecodingError.typeMismatch(let type, let context) {
+                print("Type '\(type)' mismatch:", context.debugDescription)
+                print("codingPath:", context.codingPath)
+            } catch {}
+
+            do {
+                succeed = try container.decode(Int.self, forKey: .succeed)
+
+            } catch DecodingError.typeMismatch(let type, let context) {
+                print("Type '\(type)' mismatch:", context.debugDescription)
+                print("codingPath:", context.codingPath)
+            } catch {}
+
+            companyId = try container.decode(Int.self, forKey: .companyId)
+
+            do {
+                cancelled = try container.decode(Int.self, forKey: .cancelled)
+
+            } catch DecodingError.typeMismatch(let type, let context) {
+                print("Type '\(type)' mismatch:", context.debugDescription)
+                print("codingPath:", context.codingPath)
+            } catch {}
+
+            do {
+                failed = try container.decode(Int.self, forKey: .failed)
+
+            } catch DecodingError.typeMismatch(let type, let context) {
+                print("Type '\(type)' mismatch:", context.debugDescription)
+                print("codingPath:", context.codingPath)
+            } catch {}
+
+            do {
+                templateTag = try container.decode(String.self, forKey: .templateTag)
+
+            } catch DecodingError.typeMismatch(let type, let context) {
+                print("Type '\(type)' mismatch:", context.debugDescription)
+                print("codingPath:", context.codingPath)
+            } catch {}
+
+            do {
+                modifiedBy = try container.decode(UserInfo1.self, forKey: .modifiedBy)
+
+            } catch DecodingError.typeMismatch(let type, let context) {
+                print("Type '\(type)' mismatch:", context.debugDescription)
+                print("codingPath:", context.codingPath)
+            } catch {}
+
+            total = try container.decode(Int.self, forKey: .total)
+        }
+
+        public func encode(to encoder: Encoder) throws {
+            var container = encoder.container(keyedBy: CodingKeys.self)
+
+            try? container.encodeIfPresent(createdBy, forKey: .createdBy)
+
+            try? container.encodeIfPresent(customTemplateTag, forKey: .customTemplateTag)
+
+            try? container.encodeIfPresent(modifiedOn, forKey: .modifiedOn)
+
+            try? container.encodeIfPresent(filePath, forKey: .filePath)
+
+            try? container.encodeIfPresent(trackingUrl, forKey: .trackingUrl)
+
+            try? container.encodeIfPresent(failedRecords, forKey: .failedRecords)
+
+            try? container.encodeIfPresent(createdOn, forKey: .createdOn)
+
+            try? container.encodeIfPresent(cancelledRecords, forKey: .cancelledRecords)
+
+            try? container.encodeIfPresent(stage, forKey: .stage)
+
+            try? container.encodeIfPresent(isActive, forKey: .isActive)
+
+            try? container.encodeIfPresent(succeed, forKey: .succeed)
+
+            try? container.encodeIfPresent(companyId, forKey: .companyId)
+
+            try? container.encodeIfPresent(cancelled, forKey: .cancelled)
+
+            try? container.encodeIfPresent(failed, forKey: .failed)
+
+            try? container.encodeIfPresent(templateTag, forKey: .templateTag)
+
+            try? container.encode(modifiedBy, forKey: .modifiedBy)
+
+            try? container.encodeIfPresent(total, forKey: .total)
+        }
+    }
+}
---
+++ archives/fyndFDK-iOS-Simulator.xcarchive/Products/Library/Frameworks/FyndXcframework.framework/Headers/FyndXcframework.h
@@ -0,0 +1,18 @@
+//
+//  FyndXcframework.h
+//  FyndXcframework
+//
+//  Created by Rushang Prajapati on 23/08/22.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for FyndXcframework.
+FOUNDATION_EXPORT double FyndXcframeworkVersionNumber;
+
+//! Project version string for FyndXcframework.
+FOUNDATION_EXPORT const unsigned char FyndXcframeworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <FyndXcframework/PublicHeader.h>
+
+
